be95fe9fb76bf7a21aa1f42faf56594b760a2ee4
Sean Holden <seanholden1@gmail.com>
2017-01-16 19:18:52 +0000

    Fixed spec in ruby cookbook

--- ruby/recipes/default.rb
+++ ruby/recipes/default.rb
@@ -4,14 +4,14 @@
 #
 # Copyright:: 2017, Sean Holden, All Rights Reserved.
 
-include_recipe "ruby_install::default"
+#include_recipe "ruby_install::default"
 
 rubies_dir = node['ruby']['rubies_dir']
 
 node['ruby']['versions'].each do |version|
   execute "Install ruby #{version}" do
     cwd '/home/vagrant'
-    environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'})
+    environment ({ 'HOME' => '/home/vagrant', 'USER' => 'vagrant' })
     user 'vagrant'
     creates "#{rubies_dir}/ruby-#{version}/bin/ruby"
     command "ruby-install --rubies-dir #{rubies_dir} ruby #{version}"
--- ruby/spec/unit/recipes/default_spec.rb
+++ ruby/spec/unit/recipes/default_spec.rb
@@ -19,14 +19,19 @@ describe 'ruby::default' do
       expect { chef_run }.to_not raise_error
     end
 
-    describe 'install ruby 2.4.0' do
+    describe 'install ruby' do
       let(:rubies_dir) { '/home/vagrant/.rubies' }
       let(:ruby_version) { '2.4.0' }
 
       it 'runs correct command' do
         expect(chef_run).to run_execute(
-          "ruby-install --rubies-dir #{rubies_dir}/ ruby #{ruby_version}"
-        ).with(user: 'vagrant')
+          "ruby-install --rubies-dir #{rubies_dir} ruby #{ruby_version}"
+        ).
+        with(
+          cwd: '/home/vagrant',
+          environment: { 'HOME' => '/home/vagrant', 'USER' => 'vagrant'},
+          user: 'vagrant'
+        )
       end
     end
   end
