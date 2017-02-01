b105d3c8f100263d1f993c3491c320e05fb05d68
Sean Holden <sean.holden@onthebeach.co.uk>
2017-02-01 21:08:58 +0000

    Added more blocks to postgres lookit_cookit recipe

--- postgresql/recipes/lookit_cookit.rb
+++ postgresql/recipes/lookit_cookit.rb
@@ -3,8 +3,29 @@ execute 'Create new psql user' do
   db_password = node['postgresql']['db_password']
 
   user 'root'
-  already_setup = "sudo -u postgres psql -tAc \"SELECT 1 FROM pg_roles WHERE rolname='#{db_user}'\" | grep 1"
-  not_if already_setup
+  exists = "sudo -u postgres psql -c \"SELECT * FROM pg_user WHERE usename='#{db_user}'\" | grep -c #{db_user}"
 
   command "sudo -u postgres psql -c \"CREATE USER #{db_user} WITH PASSWORD '#{db_password}';\""
+  not_if exists
+end
+
+execute 'Create new database' do
+  db_name = node['postgresql']['db_name']
+
+  user 'root'
+  exists = "sudo -u postgres psql -c \"SELECT * FROM pg_database WHERE datname='#{db_name}'\" | grep -c #{db_name}"
+
+  command "sudo -u postgres psql -c \"CREATE DATABASE #{db_name};\""
+  not_if exists
+end
+
+execute 'Grant all privileges on database to user' do
+  db_name = node['postgresql']['db_name']
+  db_user = node['postgresql']['db_user']
+
+  user 'root'
+  has_permissions = "sudo -u postgres psql -c \"SELECT has_database_privilege('#{db_user}', '#{db_name}', 'CREATE');\" | grep -c \"^ t$\""
+
+  command "sudo -u postgres psql -c \"GRANT ALL PRIVILEGES ON DATABASE #{db_name} to #{db_user}\""
+  not_if has_permissions
 end
---
+++ postgresql/spec/unit/recipes/lookit_cookit_spec.rb
@@ -0,0 +1,79 @@
+# Cookbook:: postgresql
+# Spec:: lookit_cookit
+#
+# Copyright:: 2017, Sean Holden, All Rights Reserved.
+
+require 'spec_helper'
+
+describe 'postgresql::lookit_cookit' do
+  context 'When all attributes are default, on centos7' do
+    let(:platform) { 'centos' }
+    let(:version) { '7.2.1511' }
+    let(:chef_run) {
+      ChefSpec::SoloRunner.new(platform: platform, version: version) do |node|
+        node.normal['postgresql']['db_user'] = 'test_user'
+        node.normal['postgresql']['db_password'] = 'password123'
+        node.normal['postgresql']['db_name'] = 'test_db'
+      end.converge(described_recipe)
+    }
+    let(:user_exists) { false }
+    let(:db_exists) { false }
+    let(:user_has_permissions) { false }
+
+    before do
+      stub_command("sudo -u postgres psql -c \"SELECT * FROM pg_user WHERE usename='test_user'\" | grep -c test_user").and_return(user_exists)
+      stub_command("sudo -u postgres psql -c \"SELECT * FROM pg_database WHERE datname='test_db'\" | grep -c test_db").and_return(db_exists)
+      stub_command("sudo -u postgres psql -c \"SELECT has_database_privilege('test_user', 'test_db', 'CREATE');\" | grep -c \"^ t$\"").and_return(user_has_permissions)
+    end
+
+    describe 'Create new psql user' do
+      it 'runs correct command' do
+        expect(chef_run).to run_execute(
+          "sudo -u postgres psql -c \"CREATE USER test_user WITH PASSWORD 'password123';\""
+        ).with(user: 'root')
+      end
+
+      context 'user already exists on the db' do
+        let(:user_exists) { true }
+
+        it 'does not run command' do
+          expect(chef_run).to_not run_execute("sudo -u postgres psql -c \"CREATE USER test_user WITH PASSWORD 'password123';\"")
+        end
+      end
+    end
+
+    describe 'Create new psql database' do
+      it 'runs correct command' do
+        expect(chef_run).to run_execute(
+          "sudo -u postgres psql -c \"CREATE DATABASE test_db;\""
+        ).with(user: 'root')
+      end
+
+      context 'database already exists' do
+        let(:db_exists) { true }
+
+        it 'does not run command' do
+          expect(chef_run).to_not run_execute(
+            "sudo -u postgres psql -c \"CREATE DATABASE test_db;\""
+          )
+        end
+      end
+    end
+
+    describe 'Grant privileges on database to user' do
+      it 'runs correct command' do
+        expect(chef_run).to run_execute(
+          "sudo -u postgres psql -c \"GRANT ALL PRIVILEGES ON DATABASE test_db to test_user\""
+        ).with(user: 'root')
+      end
+
+      context 'user already has permissions granted' do
+        let(:user_has_permissions) { true }
+
+        it 'does not run command' do
+          expect(chef_run).to_not run_execute("sudo -u postgres psql -c \"GRANT ALL PRIVILEGES ON DATABASE test_db to test_user\"")
+        end
+      end
+    end
+  end
+end
